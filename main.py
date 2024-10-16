ce2798a747be137e228830b02c5af658a466f12e
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-10-16 20:55:22 +0300

    Revert "Добавлен паттерн проверки фамилии и сама проверка" This reverts commit 876e41f7 Проект восстановлен

--- main.py
+++ main.py
@@ -10,3 +10,21 @@ if reg_name.match(user_name):
     print(f"Имя {user_name} принято!")
 else:
     print("Ввод некорректен! Повторите ввод данных!\n")
+
+user_surname = input("Введите фамилию пользователя: ")
+if reg_surname.match(user_surname):
+    print(f"Фамилия {user_surname} принята!")
+else:
+    print("Ввод некорректен! Повторите ввод данных!\n")
+
+user_phone = input("Введите номер телефона пользователя: ")
+if reg_name.match(user_phone):
+    print(f"Номер телефона {user_phone} принят!")
+else:
+    print("Ввод некорректен! Повторите ввод данных!\n")
+
+user_email = input("Введите e-mail пользователя: ")
+if reg_name.match(user_email):
+    print(f"E-mail {user_email} принят!")
+else:
+    print("Ввод некорректен! Повторите ввод данных!\n")
