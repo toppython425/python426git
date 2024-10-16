207994b4e37e729f75906e45380e04795ded16cb
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-10-16 20:41:21 +0300

    Добавлена паттерн номера телефона и его проверка

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import re
 
 reg_name = re.compile(r'^[A-Za-zА-Яа-яЁё]+$')
 reg_surname = re.compile(r'^[A-Za-zА-Яа-яЁё]+$')
+reg_phone = re.compile(r'^[+]\d{1,3}\(\d{1,3}\)(\d{7})$')
 
 user_name = input("Введите имя пользователя: ")
 if reg_name.match(user_name):
@@ -10,7 +11,13 @@ else:
     print("Ввод некорректен! Повторите ввод данных!\n")
 
 user_surname = input("Введите фамилию пользователя: ")
-if reg_name.match(user_surname):
+if reg_surname.match(user_surname):
     print(f"Фамилия {user_surname} принята!")
+else:
+    print("Ввод некорректен! Повторите ввод данных!\n")
+
+user_phone = input("Введите номер телефона пользователя: ")
+if reg_name.match(user_phone):
+    print(f"Номер телефона {user_phone} принят!")
 else:
     print("Ввод некорректен! Повторите ввод данных!\n")
