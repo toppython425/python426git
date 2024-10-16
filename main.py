876e41f724a335d6387c5f8c5737f1c8a0a50de8
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-10-16 20:35:59 +0300

    Добавлен паттерн проверки фамилии и сама проверка

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import re
 
 reg_name = re.compile(r'^[A-Za-zА-Яа-яЁё]+$')
+reg_surname = re.compile(r'^[A-Za-zА-Яа-яЁё]+$')
 
 user_name = input("Введите имя пользователя: ")
-
 if reg_name.match(user_name):
     print(f"Имя {user_name} принято!")
 else:
-    print("Ввод некорректен повторите! Повторите ввод данных!\n")
+    print("Ввод некорректен! Повторите ввод данных!\n")
+
+user_surname = input("Введите фамилию пользователя: ")
+if reg_name.match(user_surname):
+    print(f"Фамилия {user_surname} принята!")
+else:
+    print("Ввод некорректен! Повторите ввод данных!\n")
