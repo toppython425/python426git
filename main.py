515f5ebbfd6c732d852c30366cde662ab6a68af0
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-10-16 20:44:05 +0300

    Добавлен паттерн e-mail и его проверка

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import re
 reg_name = re.compile(r'^[A-Za-zА-Яа-яЁё]+$')
 reg_surname = re.compile(r'^[A-Za-zА-Яа-яЁё]+$')
 reg_phone = re.compile(r'^[+]\d{1,3}\(\d{1,3}\)(\d{7})$')
+reg_email = re.compile(r'^[A-Za-z0-9_]+@yandex\.ru$')
 
 user_name = input("Введите имя пользователя: ")
 if reg_name.match(user_name):
@@ -19,5 +20,11 @@ else:
 user_phone = input("Введите номер телефона пользователя: ")
 if reg_name.match(user_phone):
     print(f"Номер телефона {user_phone} принят!")
+else:
+    print("Ввод некорректен! Повторите ввод данных!\n")
+
+user_email = input("Введите e-mail пользователя: ")
+if reg_name.match(user_email):
+    print(f"E-mail {user_email} принят!")
 else:
     print("Ввод некорректен! Повторите ввод данных!\n")
