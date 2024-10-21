4d0f9b98e0a507ad5f9da6f91563ff17a3924e3a
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-10-21 18:43:44 +0300

    Обновлено reg_email

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 reg_name = re.compile(r'^[A-Za-zА-Яа-яЁё]+$')
 reg_surname = re.compile(r'^[A-Za-zА-Яа-яЁё]+$')
 reg_phone = re.compile(r'^[+]\d{1,3}\(\d{1,3}\)(\d{7})$')
-reg_email = re.compile(r'^[A-Za-z0-9_]+@yandex\.ru$')
+reg_email = re.compile(r'^[A-Za-z0-9]+@yandex\.ru$')
 
 user_name = input("Введите имя пользователя: ")
 if reg_name.match(user_name):
